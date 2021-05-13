ada7f9c86c87af20677efdb150cb91c2c38b4cf4
Yerson Carhuallanqui <yerson.rc@gmail.com>
2021-05-13 15:08:14 -0500

    Update css module

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,23 +1,21 @@
 import Link from 'next/link';
-import style from './Header-style';
-import c from 'classnames';
+import s from './Header.module.css';
 
 const linkFredd = '//fredd-project-webapp.vercel.app/';
 
 const Header = () => (
-   <div className="header">
-      <div className="header__inner">
+   <div>
+      <div className={s.header__inner}>
          <Link href="/">
-            <a className="logo">
+            <a className={s.logo}>
                <img src="images/zurvin-logo-white.svg" />
             </a>
          </Link>
 
-         <div className="menu-wrapper">
-            <Link href={linkFredd}><a className="button" target="_blank">Ingresar a Fredd</a></Link>
+         <div className={s.menuWrapper}>
+            <Link href={linkFredd}><a className={s.button} target="_blank">Ingresar a Fredd</a></Link>
          </div>
       </div>
-      <style jsx>{ style }</style>
    </div>
 )
 
