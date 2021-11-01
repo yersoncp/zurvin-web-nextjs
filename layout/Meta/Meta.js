6d30438ef483e211d9eaf8ddc7be7b897b3123d8
Yerson Carhuallanqui <yerson.rc@gmail.com>
2021-11-01 06:46:18 -0500

    Remove       <link rel=preconnect href=https://fonts.gstatic.com crossorigin />

--- layout/Meta/Meta.js
+++ layout/Meta/Meta.js
@@ -12,7 +12,6 @@ const Meta = () => (
       <meta name="theme-color" content="#080808" />
       
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100;200;300;400;500;600&display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
     </Head>
