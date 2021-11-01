bf1d6c4b1e74e330ebf290741b786daa993e1fab
Yerson Carhuallanqui <yerson.rc@gmail.com>
2021-11-01 06:54:39 -0500

    Upgrade next ffrom 10 to 11

--- params/params.js
+++ params/params.js
@@ -1,9 +1,9 @@
 const PARAMS = (
     {
         links: {
-            github: '//github.com/yersoncp',
-            linkedin: '//linkedin.com/in/yerson',
-            blog: '//blog.zurvin.com/',
+            github: 'https://github.com/yersoncp',
+            linkedin: 'https://linkedin.com/in/yerson',
+            blog: 'https://blog.zurvin.com/',
         }
     }
 )
