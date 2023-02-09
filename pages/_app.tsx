bdb0aa7922cb47c75a9851f6e268af036ac511e1
Yerson Carhuallanqui <yerson.rc@gmail.com>
2023-02-09 10:34:01 -0500

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Script from "next/script";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -20,6 +21,7 @@ function MyApp({ Component, pageProps }) {
         `}
       </Script>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
