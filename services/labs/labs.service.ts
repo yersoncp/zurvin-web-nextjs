322b00cc2d46c08461135c8b014a8e91b8ab4ebd
yersoncp <yerson.rc@gmail.com>
2023-07-08 23:07:01 -0500

    update notion

--- services/labs/labs.service.ts
+++ services/labs/labs.service.ts
@@ -6,6 +6,12 @@ export const getLabs = async (page?: number): Promise<Lab[]> => {
   const labs: LabApiResponse[] = await getDatabase({
     pageSize: page,
     databaseId: process.env.NOTION_LABS_DATABASE_ID,
+    sorts: [
+      {
+        property: "date",
+        direction: "descending"
+      }
+    ],
     filter: {
       and: [
         {
